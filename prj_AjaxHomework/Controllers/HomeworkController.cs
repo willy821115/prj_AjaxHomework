6645c33cfd56d7363adbdf4f347863c12642c1fa
willy <willy@MSI>
2023-08-07 21:23:04 +0800

    加入專案檔案。

---
+++ prj_AjaxHomework/Controllers/HomeworkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prj_AjaxHomework.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
