026b6c57f96dd111ba006e4f8af835af48710c34
willy821115 <willy821115@yahoo.com.tw>
2023-08-08 09:01:29 +0800

    加入專案檔案。

---
+++ prj_AjaxHomework/Controllers/HomeworkController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prj_AjaxHomework.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult Seven1()
+        {
+            return View();
+        }
+
+        public IActionResult Seven2()
+        {
+            return View();
+        }
+    }
+}
---
+++ prj_AjaxHomework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prj_AjaxHomework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
